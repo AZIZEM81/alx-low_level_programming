0e3cbf45872de947dc0cccfb7a28b80a183d1144
AZIZEM81 <eaziz428@gmail.com>
2023-11-23 12:30:14 -0800

    asd

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,16 +2,16 @@
 
 /**
 * malloc_checked - cause normal process termination with a status value of 98
-*
-* @b: allocated memory
-*
+* @b: int
 * Return: a pointer to the allocated memory
 */
+
 void *malloc_checked(unsigned int b)
 {
-void *i;
-i = malloc(b);
-if (i == NULL)
-exit(98);
-return (i);
+	int *m = malloc(b);
+
+	if (m == 0)
+		exit(98);
+
+	return (m);
 }
